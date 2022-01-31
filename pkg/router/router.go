91dfefef3b909d8f8a804204fe958ea368f48964
MatiasCermak <matias.cermak99@gmail.com>
2022-01-30 23:57:04 -0300

    Added OPTIONS routes

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -5,11 +5,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Run(){
+func Run() {
 	router := gin.Default()
 	router.GET("/auth", controller.Auth)
 	router.GET("/items/all", controller.ItemsAll)
 	router.POST("/items/publish", controller.PublishItem)
 	router.POST("/items/questions/ans", controller.AnswerQuestion)
+	router.OPTIONS("/items/all", controller.ItemsAll)
+	router.OPTIONS("/items/publish", controller.PublishItem)
+	router.OPTIONS("/items/questions/ans", controller.AnswerQuestion)
 	router.Run()
 }
